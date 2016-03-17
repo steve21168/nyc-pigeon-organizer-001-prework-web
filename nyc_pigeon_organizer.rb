9ca231d518d840991a88e63897eb28599cf618f2
steve21168 <steve21168@gmail.com>
2016-03-17 13:22:58 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'byebug'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  #debugger
+  data.each do |attribute, data_hash|
+    data_hash.each do |data, arr_of_names|
+      arr_of_names.each do |name|
+        data_s = data.to_s
+
+        if new_hash[name].nil?
+          new_hash[name] = { attribute => [data_s] }
+        elsif new_hash[name][attribute].nil?
+          new_hash[name][attribute] = [data_s]
+        else
+          new_hash[name][attribute] << data_s
+        end
+      end
+    end
+  end
+  new_hash
 end
